71098e8068eb2899b645151f8fcf686e27d1a0a5
Zack Mance <zmance@gmail.com>
2015-04-27 16:06:21 -0400

    Adds User Migration

--- db/migrate/20141020120711_create_users.rb
+++ db/migrate/20141020120711_create_users.rb
@@ -1,8 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :password_digest
+      t.string :first_name, null: false
+      t.string :last_name,  null: false
+      t.string :email,      null: false
+      t.integer :age,       null: false
+      t.timestamps null: false
     end
   end
 end
