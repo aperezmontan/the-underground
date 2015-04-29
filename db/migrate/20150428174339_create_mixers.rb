621a554f4f9bcd91caa856efca885489a6e80b88
Zack Mance <zmance@gmail.com>
2015-04-29 12:23:08 -0400

    Makes all the new Migrations

--- db/migrate/20150428174339_create_mixers.rb
+++
@@ -1,9 +0,0 @@
-class CreateMixers < ActiveRecord::Migration
-  def change
-    create_table :mixers do |t|
-      t.string :name,   null: false
-      t.decimal :price, null: false
-      t.timestamps      null: false
-    end
-  end
-end
--- db/migrate/20150428174344_create_bottles.rb
+++
@@ -1,8 +0,0 @@
-class CreateBottles < ActiveRecord::Migration
-  def change
-    create_table :bottles do |t|
-      t.string  :name,  null: false
-      t.decimal :price, null: false
-    end
-  end
-end
