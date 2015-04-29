1f161710fb6b41d22554d87d840a20bacc11d587
Zack Mance <zmance@gmail.com>
2015-04-29 11:56:51 -0400

    Renames VIP table to Reservations

--- db/migrate/20150428174325_create_reservations.rb
+++ db/migrate/20150428174325_create_reservations.rb
@@ -1,6 +1,6 @@
-class CreateVips < ActiveRecord::Migration
+class CreateReservations < ActiveRecord::Migration
   def change
-    create_table :vips do |t|
+    create_table :reservations do |t|
       t.integer     :event_id,  null: false
       t.integer     :user_id,   null: false
       t.timestamps              null: false
