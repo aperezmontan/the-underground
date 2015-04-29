ad00e6d5e47c9a1bbb3351dfb506e59fcdfa1249
Zack Mance <zmance@gmail.com>
2015-04-29 14:48:34 -0400

    Creates Poloymorphaic Items / bottle / mixers

--- db/migrate/20150429121254_create_items.rb
+++ db/migrate/20150429121254_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration
     create_table :items do |t|
       t.string      :type,  null: false
       t.string      :name,  null: false
-      t.decimal     :price, null: false, precision: 2
+      t.decimal     :price, null: false, precision: 8, scale: 2
       t.timestamps          null: false
     end
   end
