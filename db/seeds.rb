8ef831bbdc595a7ff93df86b81d17773fa8c7f2e
Zack Mance <zmance@gmail.com>
2015-04-29 15:39:32 -0400

    Adds Checks Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@ guest_2 = User.create(first_name: 'Scott', last_name: 'Summers', email: 'zack@db
 graduation = Event.create(name: 'Graduation', min_age: 18, date_of: DateTime.parse('05/06/2015'), organizer_id: organizer.id)
 
 
+resi_1 = Reservation.create(user_id: guest_1.id, event_id: graduation.id)
+resi_2 = Reservation.create(user_id: guest_2.id, event_id: graduation.id)
+
 gg = Bottle.create(name: 'Blue Goose', price: 500.00)
 gj = Bottle.create(name: 'Gopher Juice', price: 1200.00)
 pp = Bottle.create(name: 'Pocket Punch', price: 2200.00)
@@ -13,3 +16,19 @@ pp = Bottle.create(name: 'Pocket Punch', price: 2200.00)
 oj = Mixer.create(name: 'Orange Juice', price: 20.00)
 cb = Mixer.create(name: 'Cranberry Juice', price: 30.00)
 sd = Mixer.create(name: 'Soda', price: 15.00)
+
+check_1 = Check.new(reservation_id: resi_1.id)
+check_2 = Check.new(reservation_id: resi_2.id)
+
+check_1.items << gg
+check_1.items << gj
+check_1.items << pp
+check_1.items << oj
+check_1.items << oj
+check_1.items << cb
+
+check_2.items << pp
+check_2.items << sd
+
+check_1.save
+check_2.save
