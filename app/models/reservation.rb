6b282e2a197d153abd3f68c1f25baefb8e1532e5
Zack Mance <zmance@gmail.com>
2015-04-29 16:12:00 -0400

    Adds Methods to calculate the total bill

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,13 @@
 class Reservation < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
-  has_many   :checks
+  has_one :check
+
+  def bottles
+   @bottles ||= check.items.where(type: 'Bottle')
+  end
+
+  def mixers
+   @mixers  ||= check.items.where(type: 'Mixer')
+  end
 end
