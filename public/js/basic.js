493006d2c7dc5450feefa347b3c4094596576f0e
Zack Mance <zmance@gmail.com>
2015-05-03 13:44:28 -0400

    Adds variables and naming

---
+++ public/js/basic.js
@@ -0,0 +1,19 @@
+//Part 1: Variables and Naming
+
+var foo = 100;
+console.log('foo=' + foo);
+
+var bar = "200"; 
+console.log('bar=' + bar);
+
+var fooBar = foo + bar; 
+console.log('foo+bar=' + fooBar);
+
+var barFoo = bar + foo;
+console.log('bar+foo=' + barFoo);
+
+var fooHundo = foo * 2;
+console.log('foo * 2=' + fooHundo);
+
+var barHundo = bar * 2;
+console.log('bar * 2=' + barHundo);
