d90f282222d5483b98f8fbd4eae12e1bbfc1fa0d
Zack Mance <zmance@gmail.com>
2015-05-03 14:09:44 -0400

    Adds Equality stuff

--- public/js/basic.js
+++ public/js/basic.js
@@ -17,3 +17,27 @@ console.log('foo * 2=' + fooHundo);
 
 var barHundo = bar * 2;
 console.log('bar * 2=' + barHundo);
+
+//Part : Equality
+var one = 1;
+var stringOne = "1";
+
+console.log("one == one " + (one == one) )
+console.log("one == stringOne " + (one == stringOne) )
+console.log("1 === stringOne " + (1 === stringOne) )
+
+if(true)
+  console.log("true is true")  
+
+if('true')
+  console.log("'true' is true");
+
+if(7)
+  console.log("int > 0 is true");
+
+if([])
+  console.log('[] array is true');
+
+if(!([].length))
+  console.log('[].length == 0 is false');
+ 
